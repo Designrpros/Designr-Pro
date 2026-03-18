a87a6c4dd2b42573f4c4c23c59768d55ca820364
Vegar <vegar@epost.no>
2026-03-18 07:35:50 +0100

    Remove: Phone number completely

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -214,7 +214,6 @@ export default function Contact() {
             Email: <ContactLink href="mailto:vegarberentsen@gmail.com">designrpros@gmail.com</ContactLink>
           </ContactInfo>
           <ContactInfo>
-            // Phone: +47 485 96 755 (see contact form)
           </ContactInfo>
           <ContactInfo>Address: Ovenbakken 31 A, Østerås, Norway</ContactInfo>
         </SectionContent>
